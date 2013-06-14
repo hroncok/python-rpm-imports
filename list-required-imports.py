e9de09288f2375e91a88c2527cbc20b66eab4399
Miro Hrončok <miro@hroncok.cz>
2013-06-14 05:56:26 +0200

    List all imported modules and classes (should not be that way)

--- list-required-imports.py
+++ list-required-imports.py
@@ -1,26 +1,32 @@
-import re
-
-f = open('input.txt','r')
-text = f.read()
-f.close()
+import re, os, sys
 
 modules = set()
 
-# import foo, bar
-match = re.findall( r'^(?: *|\t*)import[ |\t]+([\w|-|,| |\t]*)$', text, re.M|re.I)
-for line in match:
-   line = line.split(',')
-   modules.update([x.strip() for x in line])
+for root, subs, files in os.walk(sys.argv[1]):
+   for file in files:
+      try:
+         with open(os.path.join(root,file)) as f:
+            text = f.read()
+      except:
+         pass
+      
+      # import foo, bar
+      match = re.findall( r'^(?: *|\t*)import(?: |\t)+([\w|-|\.|,| |\t]*)$', text, re.M|re.I)
+      for line in match:
+         line = line.split(',')
+         modules.update([x.strip() for x in line])
 
-# from foo import bar
-match = re.findall( r'^(?: *|\t*)from[ |\t]+(\w|-)+[ |\t]+import\s+([\w|-|,| |\t]*)$', text, re.M|re.I)
-for line in match:
-   line = line[0], line[1].split(',')
-   modules.update([line[0].strip()+'.'+x.strip() for x in line[1]])
+      # from foo import bar
+      match = re.findall( r'^(?: *|\t*)from(?: |\t)+((?:\w|-|\.)+)(?: |\t)+import\s+([\w|-|\.|,| |\t]*)$', text, re.M|re.I)
+      for line in match:
+         line = line[0], line[1].split(',')
+         modules.update([line[0]+'.'+x.strip() for x in line[1]])
 
-# from foo import *
-match = re.findall( r'^(?: *|\t*)from[ |\t]+(\w|-)+[ |\t]+import\s+\*$', text, re.M|re.I)
-modules.update(match)
-   
-for module in modules:
+      # from foo import *
+      match = re.findall( r'^(?: *|\t*)from(?: |\t)+((?:\w|-|\.)+)(?: |\t)+import\s+\*$', text, re.M|re.I)
+      modules.update(match)
+
+
+
+for module in sorted(modules):
    print(module)
