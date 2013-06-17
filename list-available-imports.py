2740cb828aec326fb16757b1d7a0f8112a6adeed
Miro Hrončok <miro@hroncok.cz>
2013-06-17 15:10:28 +0200

    Do not list namespaces as modules

--- list-available-imports.py
+++ list-available-imports.py
@@ -1,5 +1,5 @@
 # this should be called with %{buildroot}%{python_sitelib} as argument
-import sys, os, pkgutil
+import sys, os, pkgutil, re
 
 # Go to the given directory and add it to sys.path
 os.chdir(sys.argv[1])
@@ -7,4 +7,15 @@ sys.path = ['.'] + list(filter(lambda x: 'site-packages' not in x, sys.path))
 
 # List all available modules in that directory
 for importer, modname, ispkg in pkgutil.walk_packages(path='.'):
+    # if it is a directory/package, check if it's not just a namespace
+    if ispkg:
+        try:
+            with open('./'+modname.replace('.','/')+'/__init__.py','r') as f:
+                init_source = f.read()
+            if re.search(r'declare_namespace\s*\(\s*__name__\s*\)',init_source):
+                continue
+            if re.search(r'extend_path\s*\(\s*__path__\s*,\s*__name__\s*\)',init_source):
+                continue
+        except IOError:
+            pass
     print(modname)
