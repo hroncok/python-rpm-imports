ca2c049d19faf8f772afc1a7ed2b0ffdffa47c39
Miro Hrončok <miro@hroncok.cz>
2013-06-17 11:40:03 +0200

    Better sys.path handling

--- list-available-imports.py
+++ list-available-imports.py
@@ -3,7 +3,7 @@ import sys, os, pkgutil
 
 # Go to the given directory and add it to sys.path
 os.chdir(sys.argv[1])
-sys.path += ['.']
+sys.path = ['.'] + list(filter(lambda x: 'site-packages' not in x, sys.path))
 
 # List all available modules in that directory
 for importer, modname, ispkg in pkgutil.walk_packages(path='.'):
