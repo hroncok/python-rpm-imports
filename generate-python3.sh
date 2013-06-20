f8b698d43ad7953c083fb4c886b39df2ff4b8294
Miro Hrončok <miro@hroncok.cz>
2013-06-21 01:08:49 +0200

    In this state, it works for non-namespaced packages

--- generate-python3.sh
+++ generate-python3.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-cp python{,3}-imports.prov
-sed -i 's/python/python3/g' python3-imports.prov
+cp python{,3}import.prov
+cp python{,3}import.attr
+sed -i 's/python/python3/g' python3import.*
+sed -i 's/python32/python3/g' python3import.*
