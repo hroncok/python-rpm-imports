bcf525bc3570a3119435618ebd8cc3ae7d4efce7
Miro Hrončok <miro@hroncok.cz>
2013-06-20 23:42:30 +0200

    Handle generate script better

--- generate-python3.sh
+++ generate-python3.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 cp python{,3}-imports.prov
-sed -i 's|#!/usr/bin/python|#!/usr/bin/python3|' python3-imports.prov
-sed -i 's|%{python_sitelib}|%{python3_sitelib}|' python3-imports.prov
+sed -i 's/python/python3/g' python3-imports.prov
